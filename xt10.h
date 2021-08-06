34e15c701f235f4a59fd38d9c6471b0b6d159b5c
Daniel Cook <brikbusters@gmail.com>
2021-08-06 13:24:53 -0400

    Correct X-T10 string offset

--- xt10.h
+++ xt10.h
@@ -5,14 +5,16 @@ Fujifilm X-T10
 
 #define MODEL_NAME "Fujifilm X-T10"
 
+// Where the code that references "out of memory" is
+#define OUT_OF_MEMORY_CODE 0x00bd8954
+
 // Location where code thinks "out of memory" is
-#define OUT_OF_MEMORY_REF 0x00605f8c
+#define OUT_OF_MEMORY_REF 0x1f54c3c
 
 // Where "out of memory" actually resides, in the
 // file that dump.c generates
 #define OUT_OF_MEMORY_REAL 0x00bf4c84
 
-
-#define MEM_START 0x00605f8c - 10000
-#define TEXT_START 0x00bf4c84 - 10000
-#define COPY_LENGTH MEM_START - TEXT_START
+#define MEM_START OUT_OF_MEMORY_REF - 10000
+#define TEXT_START OUT_OF_MEMORY_REAL - 10000
+#define COPY_LENGTH 10000 + 6052
