bd1af907e122309b0b488912cd655db372c84e1e
Daniel C <brikbusters@gmail.com>
2022-01-20 20:14:55 -0500

    util.mk: importMacro A way of importing macros from C header files right into GNU Make. No sed/grep/cat nonsense, just creates a C program that creates a Make expression to declare the variable.
    Simple, genuis.

---
+++ src/util.mk
@@ -0,0 +1,11 @@
+# "Parse" C header, converting C macro to Make macro
+# See https://gist.github.com/petabyt/898d3437decf65fc04fc50bd0e125362
+# Usage:
+# $(call getMacro header.h MY_MAC %s)
+define importMacro
+    $(shell echo "#include <stdio.h>\n \
+        int main() {printf(\"$(2)=$(3)\", $(2));return 0;}" > temp.c)
+    $(shell $(CC) -include $(1) temp.c -o temp.o)
+	$(eval $(shell ./temp.o))
+    $(shell $(RM) temp.c temp.o)
+endef
