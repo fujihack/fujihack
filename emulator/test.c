6dc012559e85348662905d2367a638d4e2a596af
Daniel C <brikbusters@gmail.com>
2022-01-22 22:10:50 -0500

    Fix emulator C example, still position dependent code In other words, back to where I was ~5 months ago.

--- emulator/test.c
+++ emulator/test.c
@@ -1,25 +1,16 @@
-asm(".code 32");
-asm(".arm");
-asm(".global _start");
-
-// Entry trampoline for functions under
-// start()
-unsigned long start();
-void _start() {
-	start();
-	((char *)0x2101168)[0] = 'Z';
+// Entry trampoline
+void *start();
+void *_start() {
+	return start();
 }
 
 // Test a function in firmware
-unsigned int test();
-__asm__(".global test\ntest:\n"
-		"b 0x0072f90c\n");
+unsigned int _strcpy();
 
 char buffer[10];
 
 // Code booter
-unsigned long start() {
-	test(buffer, "asd");
+void *start() {
+	_strcpy(buffer, "asd");
 	return buffer;
 }
-
