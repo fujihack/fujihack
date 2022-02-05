8ddb5d8c40b9daf8b5be83d4dfbda92a649c6382
Daniel C <brikbusters@gmail.com>
2022-02-04 20:01:56 -0500

    Work on screen hacks some still don't fully understand screen buffer, seems to wrap around or something.

--- src/test.c
+++ src/test.c
@@ -1,5 +1,3 @@
-// This code is injected into the Fujifilm camera
-
 #ifndef MODEL_NAME
 	#include "../model/xf1.h"
 #endif
@@ -7,21 +5,17 @@
 #include <string.h>
 #include "sqlite.h"
 
-int dump_test();
-char get_drive();
-int fuji_fopen();
-
-// screen buffer testing
-#define BUF_3 0x1cef200 - 0x82 - (640 * 4)
-#define BUF_2 0x1cee77e + ((648 * 480) * -1) + 50 - 3 + 1800 - 22
-
-#define BUF 0x1cee77e + ((648 * 480) * -1)
-
 #define WIDTH 640
+#define HEIGHT 480
+
+#define BUF2 0x1cee77e + ((WIDTH * HEIGHT) * 0)
+
+#define BUFFER 0x1ce8bee - (WIDTH * HEIGHT)
 
 void pixel(int x, int y) {
-	int *buf = (int *)BUF;
-	buf[WIDTH * y + 1 + x] = 0x0;
+	int *buf = (int *)BUFFER;
+	
+	buf[WIDTH * y + x] = 0x0;
 }
 
 void rect(int x1, int y1, int w1, int h1) {
@@ -33,16 +27,14 @@ void rect(int x1, int y1, int w1, int h1) {
 }
 
 void entry() {
-	char *model = (char *)MEM_MODEL_TEXT;
-	sqlite_snprintf(0x20, model, "Test: 0x%x", ((unsigned int *)0x01628656)[0]); // 0x28b00d8
-
-	int *fudge = (int *)BUF;
+	int *fudge = (int *)BUFFER;
 
 	for (int i = 0; i < 640 * 480; i++) {
 		fudge[i] = 0xffffffff;
 	}
 
-	rect(0, 50, 100, 100);
-	rect(0, 200, 100, 100);
+	rect(0, 0, 100, 100);
+	rect(100, 100, 100, 100);
+	rect(200, 200, 100, 100);
 	rect(300, 300, 100, 100);
 }
