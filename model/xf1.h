63000edf59fc03d35b9c958bf8873c9783e2d75a
Daniel C <brikbusters@gmail.com>
2022-02-04 20:00:46 -0500

    Fix sqlite_exec stub

--- model/xf1.h
+++ model/xf1.h
@@ -41,7 +41,7 @@ Fujifilm X-F1 Information File
 	NSTUB(fuji_fread, 0x0072b618)
 	NSTUB(fuji_malloc, 0x0073a2cc)
 
-	NSTUB(sqlite_exec, 0x014351ec)
+	NSTUB(sqlite_exec, 0x014224b4)
 	NSTUB(sqlite_snprintf, 0x013ff32c)
 	NSTUB(sqlite_malloc, 0x013fddcc) // aka sqlite3Malloc, does not seem to work at all
 
