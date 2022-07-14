a481fa8dd643db147c6eb6209a230884a8b2bd75
Daniel C <brikbusters@gmail.com>
2022-07-14 03:47:34 -0400

    Finally figured out file writing for Fujifilm cameras. very confusing API, makes no sense at all. Is it bad design? Is it genius design? Is it a replica of another operating system? I may never know. Added a RAM dumper though, and it works FAST. It took me 5 hours to dump 30mb from PTP/USB, and it only takes around 20 seconds to dump 256mb to the SD card. This is a great help.

--- src/fujifilm.h
+++ src/fujifilm.h
@@ -9,6 +9,7 @@ last 2 bytes are transparency (????)
 
 */
 
+// Screen
 #define COL_BLACK 0x00000000
 #define COL_WHITE 0xffffffff
 
@@ -18,6 +19,23 @@ unsigned int fuji_malloc(int n, void *addr, int mode);
 
 // Returns current drive (DOS style)
 char fuji_drive();
+
+// Screen text
+#define TEXT_BLACK 7
+#define TEXT_WHITE 1
+
+// Write ASCII text rows to screen
+void fuji_screen_write(char string[], int x, int y, int foreground_color, int background_color);
+
+// Delete text buffer, does not take effect until screen updates
+void fuji_discard_text_buffer();
+
+void *fuji_fopen(uint32_t handler, char string[]);
+void *fuji_fwrite(uint32_t handler, void *fp, int n, void *data);
+void *fuji_fclose(uint32_t handler, void *fp, int x, char *y);
+
+void fuji_toggle();
+void fuji_zero();
 
 #if 0
 // Posix wrappers around RTOS functions:
--- src/main.c
+++ src/main.c
@@ -52,11 +52,49 @@ int printChar(int x, int y, char c) {
 }
 #endif
 
+#if 1
+
+// 512 meg
+#define DUMP_SIZE_MB 256
+#define DUMP_SIZE DUMP_SIZE_MB * 1000 * 1000
+
+void memory_dump() {
+	char buffer[16];
+	sqlite_snprintf(16, buffer, "Dumping %uMB RAM", DUMP_SIZE_MB);
+	fuji_screen_write(buffer, 1, 1, 0, 7);
+	fuji_screen_write("Will lock up", 1, 2, 0, 7);
+
+	uint8_t *mem = (uint8_t*)0x0;
+
+	char file[] = "X:\\RAM.BIN";
+	file[0] = fuji_drive();
+
+	fuji_toggle();
+	void *fp = fuji_fopen(FUJI_FOPEN_HANDLER, file);
+	fuji_toggle();
+	fuji_zero();
+
+	fuji_toggle();
+	fuji_fwrite(FUJI_FWRITE_HANDLER, fp, DUMP_SIZE, mem);
+	fuji_toggle();
+	fuji_zero();
+
+	fuji_toggle();
+	fuji_fclose(FUJI_FCLOSE_HANDLER, fp, 0, (char*)0);
+	fuji_toggle();
+	fuji_zero();
+
+	fuji_screen_write("Done.", 1, 3, 0, 7);
+}
+#endif
+
 void entry() {
-	int buf;
-	int r = fuji_malloc(0x014559c8, &buf, -1);
-
-	char *a = (char*)MEM_MODEL_TEXT;
-
-	sqlite_snprintf(40, a, "E %d", r);
+// Basic Test
+#if 0
+	char *x = (char*)MEM_MODEL_TEXT;
+	sqlite_snprintf(16, x, "Hello World");
+#endif
+#if 0
+	memory_dump();
+#endif
 }
