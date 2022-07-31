56dd480d02ee9a96c071ed3a1dd58ca509d77745
Daniel C <brikbusters@gmail.com>
2022-07-31 00:45:43 -0400

    Some XF1 stubs

--- model/xf1_101.h
+++ model/xf1_101.h
@@ -11,10 +11,6 @@ Fujifilm X-F1 Information File
 #define PRINTIM_HACK_WORKS
 #define MEMO_HACK_WORKS
 
-#define MEM_START 0x011ea108 - 0x10000
-#define TEXT_START 0x00492150 - 0x10000
-#define COPY_LENGTH 0x500000
-
 // Code that writes "PrintIM" to JPEG images. A safe place
 // To execute code.
 #define FIRMWARE_PRINTIM 0x00516c90
@@ -48,20 +44,30 @@ Fujifilm X-F1 Information File
 #define FIRM_DUMP_ADDR 0x006e3748
 #define MEM_DUMP_ADDR 0x0143b700
 
+// Where "corrupted" firmware data starts
+#define MEM_FIRM_START 0x96b10c0
+
+// EEPRom data
+#define MEM_EEP_START 0x409a7e00
+
 // "Seems to work" screen buffer
 #define SCREEN_WIDTH 640
 #define SCREEN_HEIGHT 480
 
-#define SCREEN_BUFFER (0x01901800 + (640*480*4) * 1) - SCREEN_WIDTH * 300 - 100
+#define MEM_SCREEN_BUFFER (0x01901800 + (640 * 4 * 170) - 520)
+#define GET_SCREEN_LAYER(x) (MEM_SCREEN_BUFFER + (SCREEN_WIDTH * SCREEN_HEIGHT * 4) * x)
 
 #define MEM_DEV_FLAG 0x007a117c
 #define MEM_DEV_MODE 0x007a7250
 
 #define FUJI_FOPEN_HANDLER 0x00fd45b4
 #define FUJI_FWRITE_HANDLER 0x00fd462c
+#define FUJI_FREAD_HANDLER 0x00e8e754
 #define FUJI_FCLOSE_HANDLER 0x00fd45dc
 
-#define MEM_INPUT_MAP 0x00795370;
+#define MEM_INPUT_MAP 0x00795370
+#define MEM_LAYER_INFO 0x0152e0f4
+#define MEM_SQLITE 0x0144c670
 
 #ifdef STUBS
 	#include "stub.h"
@@ -78,14 +84,12 @@ Fujifilm X-F1 Information File
 	NSTUB(fuji_toggle, 0x00fd5a1c)
 	NSTUB(fuji_zero, 0x00fd4590)
 
-	/* Test:
-	sqlite_exec(0x0144c67c, "create table foo()", ); 
-	*/
+	NSTUB(fuji_create_fixedmemory, 0x0073a4f4)
 
-	NSTUB(sqlite_parse, 0x01435224) // not entirely sure
+	NSTUB(fuji_init_sqlite, 0x013c24a8)
 	NSTUB(sqlite_exec, 0x014224b4)
 	NSTUB(sqlite_snprintf, 0x013ff32c)
-	NSTUB(sqlite_malloc, 0x013fddcc) // aka sqlite3Malloc, does not seem to work at all
+	NSTUB(sqlite_malloc, 0x013fddcc)
 
 	NSTUB(random_strcpy, 0x0072f90c) // good emulator testing function
 	NSTUB(random_strncat, 0x0072f9d8)
@@ -97,22 +101,33 @@ Fujifilm X-F1 Information File
 	NSTUB(fuji_task_sleep, 0x0073ba3c)
 	NSTUB(fuji_create_semaphore, 0x0073b3a4)
 
+	// Experimental:
+
 	NSTUB(fuji_task_create, 0x00735c2c)
 	NSTUB(fuji_task_check, 0x00734610)
 	NSTUB(fuji_task_test, 0x00734848)
 
-	NSTUB(test, 0x00e5bcd4)
 	NSTUB(fuji_get_key, 0x00d17d4c)
 
-	NSTUB(fuji_diropen, 0x00e4d8d0)
-
-	NSTUB(FUN_0073910c, 0x0073910c)
+	NSTUB(fuji_diropen, 0x0070ae18) // ?
 
 	NSTUB(sensor_info, 0x00fee158)
-
 	NSTUB(key_push, 0x011d2650)
-
 	NSTUB(run_auto_act, 0x00fd5aa4)
+
+	NSTUB(fuji_ptp_return, 0x00e507c0)
+	NSTUB(fuji_ptp_finish, 0x00e514f4)
+
+	NSTUB(fuji_show_gui, 0x00e13030)
+	NSTUB(fuji_beep, 0x00e14d18)
+
+
+	NSTUB(FUN_00e12b24, 0x00e12b24)
+	NSTUB(FUN_00debcc8, 0x00debcc8)
+
+	// uint32_t io[3];
+	// get_io(io, MEM_INPUT_MAP, 10);
+	NSTUB(get_io, 0x0072fe08)
 #endif
 
 // Not working
