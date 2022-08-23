b4621f414391b193f39c20ef074a6a6ab0caa488
Daniel C <brikbusters@gmail.com>
2022-08-23 17:18:59 -0400

    Revamp "boot" process entry.S -> boot.S remove junk code from main.c

--- src/main.c
+++ src/main.c
@@ -11,47 +11,20 @@
 #include "sqlite.h"
 #include "screen.h"
 #include "hijack.h"
-
-#define DUMP_SIZE_MB 256
-#define DUMP_SIZE DUMP_SIZE_MB * 1000 * 1000
-
-int bg_x = 0;
-void memory_dump(char file[], uintptr_t location) {
-	char buffer[16];
-	sqlite_snprintf(16, buffer, "Dumping %uMB RAM", DUMP_SIZE_MB);
-	fuji_screen_write(buffer, 1, 1, 0, 7);
-	fuji_screen_write("Will lock up", 1, 2, 0, 7);
-
-	uint8_t *mem = (uint8_t*)location;
-
-	file[0] = fuji_drive();
-
-	fuji_toggle();
-	void *fp = fuji_fopen(FUJI_FOPEN_HANDLER, file, 1);
-	fuji_toggle();
-	fuji_zero();
-
-	fuji_toggle();
-	fuji_fwrite(FUJI_FWRITE_HANDLER, fp, DUMP_SIZE, mem);
-	fuji_toggle();
-	fuji_zero();
-
-	fuji_toggle();
-	fuji_fclose(FUJI_FCLOSE_HANDLER, fp, 0, (char*)0);
-	fuji_toggle();
-	fuji_zero();
-
-	fuji_screen_write("Done.", 1, 3, 0, 7);
-}
-
 #include "parasite.h"
 
-struct Font {
-    char letter;
-    char code[7][5];
-};
+void entry() {
+#if 0
+	int r = fuji_dir_open("A:\\", "*", buffer);
+	r = fuji_dir_next(buffer);
+#endif
+#if 0
+	write_file("X:\\DCAA\\auto_act.scr",
+		"WAITSET 123\r\n"
+	);
 
-void entry(uintptr_t base) {
+	run_auto_act();
+#endif
 #if 0
 	fujihack_init(base);
 #endif
