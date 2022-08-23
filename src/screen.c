f9637f8689bf2a9d86afbccd960113f392b3d891
Daniel C <brikbusters@gmail.com>
2022-08-23 17:17:44 -0400

    Remove 5x7 font code from screen.c, add console_log()

--- src/screen.c
+++ src/screen.c
@@ -23,56 +23,18 @@ void disp_rect(int x1, int y1, int w1, int h1, int col) {
 	}
 }
 
-// Testing read, alloc, in font binary
-#ifdef STANDALONE_FONT_TEST
-int font_print_char(struct Font *f, int x, int y, char c) {
-	// Loop to "null terminator character"
-	int match = 0;
-	for (int l = 0; f[l].letter != 0; l++) {
-		if (f[l].letter == c) {
-			match = l;
-			break;
-		}
+#define CONSOLE_MAX 10
+
+int console_curr = 0;
+
+void console_log(char string[]) {
+	fuji_screen_write(string, 1, 1 + console_curr, TEXT_BLACK, TEXT_WHITE);
+	console_curr++;
+
+	if (console_curr > 9) {
+		console_curr = 0;
+		fuji_discard_text_buffer();
 	}
-
-	// Loop through 7 high 5 wide monochrome font
-	int maxLength = 0;
-	for (int py = 0; py < 7; py++) {
-		for (int px = 0; px < 5; px++) {
-			if (f[match].code[py][px] == '#') {
-				disp_pixel(x, y, 0xffffffff);
-
-				// Dynamix width character spacing
-				if (px > maxLength) {
-					maxLength = px;
-				}
-			}
-		}
-	}
-
-	return maxLength;
 }
 
-void testfont() {
-	fuji_init_sqlite();
-	struct Font *font = (struct Font*)sqlite_malloc(3276);
-
-	char file[] = "X:\\font.bin";
-	file[0] = fuji_drive();
-	FILE *fp = fopen(file, 0);
-	fread(font, 3276, 1, fp);
-	fclose(fp);
-
-	font_print_char(font, 10, 10, 'A');
-}
-#endif
-
-// Testing 3x3 font stuff
-#ifdef INCLUDE_FONT_STUFF
-void font_draw_pixel(int x, int y) {
-	disp_pixel(x, y);
-}
-
-#include "font/font.h"
-#include "font/lib.c"
 #endif
