400e3b9384bda4d16204f565fbdc0039a6092355
Daniel Cook <brikbusters@gmail.com>
2023-01-06 18:48:36 -0800

    Add stubs to X-A2 1.30

--- model/xa2_130.h
+++ model/xa2_130.h
@@ -7,15 +7,17 @@ https://en.wikipedia.org/wiki/Fujifilm_X-A2
 
 #define MODEL_NAME "Fujifilm X-A2"
 #define MODEL_CODE "00050701000507020005070400050709"
+#define FIRM_URL "https://dl.fujifilm-x.com/support/firmware/x-a2-130-oqijkawt/FWUP0006.DAT?_ga=2.22183931.1717134919.1673059540-714234086.1665772471"
 #define CODE_ARM
 
 #define S3_FILE "C:\\IMFIDX10\\LX30B.S3"
 
-#define FIRM_IMG_PROPS 0x01553b1b
+#define FIRM_IMG_PROPS 0x00598aec
 #define FIRM_IMG_PROPS_MAX 2000
+#define FIRM_RST_WRITE 0x005b0388
+#define FIRM_RST_CONFIG1 0x00592bd4
+#define FIRM_RST_CONFIG2 0x005b81e0
 
 #ifdef STUBS
 	#include "stub.h"
-
-	//NSTUB(show_photo_properties, 0x01553b1b) // firm stub or RAM stub???
 #endif
