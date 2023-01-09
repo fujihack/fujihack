b00baed93f8cc0f589eb0c8ba1860d94ade56d95
Daniel Cook <brikbusters@gmail.com>
2023-01-09 14:46:46 -0800

    Add X-A2 direct PTP stubs

--- model/xa2_130.h
+++ model/xa2_130.h
@@ -7,16 +7,20 @@ https://en.wikipedia.org/wiki/Fujifilm_X-A2
 
 #define MODEL_NAME "Fujifilm X-A2"
 #define MODEL_CODE "00050701000507020005070400050709"
-#define FIRM_URL "https://dl.fujifilm-x.com/support/firmware/x-a2-130-oqijkawt/FWUP0006.DAT?_ga=2.22183931.1717134919.1673059540-714234086.1665772471"
+#define FIRM_URL "https://dl.fujifilm-x.com/support/firmware/x-a2-130-oqijkawt/FWUP0006.DAT"
 #define CODE_ARM
-
 #define S3_FILE "C:\\IMFIDX10\\LX30B.S3"
 
+// Dead code (doesn't work in camera, not in manual)
 #define FIRM_IMG_PROPS 0x00598aec
 #define FIRM_IMG_PROPS_MAX 2000
 #define FIRM_RST_WRITE 0x005b0388
 #define FIRM_RST_CONFIG1 0x00592bd4
 #define FIRM_RST_CONFIG2 0x005b81e0
+
+#define FIRM_PTP_9805 0x008722f8
+#define FIRM_PTP_FINISH 0x0087b94c
+#define FIRM_PTP_MAX 3000
 
 #ifdef STUBS
 	#include "stub.h"
