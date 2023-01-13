cb53b717601b0099db61a44d307d25bffbf74f82
Daniel Cook <brikbusters@gmail.com>
2023-01-12 18:48:44 -0800

    Add some initial xa2 RAM stubs

--- model/xa2_130.h
+++ model/xa2_130.h
@@ -22,6 +22,23 @@ https://en.wikipedia.org/wiki/Fujifilm_X-A2
 #define FIRM_PTP_FINISH 0x0087b94c
 #define FIRM_PTP_MAX 3000
 
+
 #ifdef STUBS
 	#include "stub.h"
+
+	NSTUB(fuji_drive, 0x006f3f08)
+	NSTUB(fuji_fopen, 0x00712178)
+	NSTUB(fuji_fread, 0x006f0efc)
+	NSTUB(fuji_fclose, 0x006f0fbc)
+	NSTUB(fuji_fwrite, 0x006f0f5c)
+
+	NSTUB(fuji_file_wait, 0x00ea95e4)
+	NSTUB(fuji_reset, 0x00ea966c)
+
+	NSTUB(fuji_screen_write, 0x010f0904)
+	NSTUB(fuji_discard_text_buffer, 0x00d3570c)
+
+	NSTUB(fuji_rst_write, 0x01110340)
+	NSTUB(fuji_rst_config1, 0x010f2b8c)
+	NSTUB(fuji_rst_config2, 0x01118198)
 #endif
